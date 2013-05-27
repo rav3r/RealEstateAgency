0c998aa5eb86d20d21aae0240caecc7cdcbf24bb
raver <rafal.gawel@gmail.com>
2013-05-27 08:55:33 +0200

    Rename project

---
+++ RealEstateAgency/src/main/java/org/rea/PostgresConfig.java
@@ -0,0 +1,9 @@
+package org.rea;
+
+// TODO: tutaj wpisujemy dane do swojej bazy danych PostgreSQL
+
+public class PostgresConfig {
+    public static String url        = "jdbc:postgresql://localhost/postgres";
+    public static String user       = "rafal";
+    public static String password   = "postgre";
+}
