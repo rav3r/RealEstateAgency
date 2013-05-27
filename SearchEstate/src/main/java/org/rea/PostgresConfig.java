820439271806f8416b6c9f6f10c309d5bd9a4d55
raver <rafal.gawel@gmail.com>
2013-05-27 10:11:52 +0200

    Adding admin panel skeleton

--- SearchEstate/src/main/java/org/rea/PostgresConfig.java
+++
@@ -1,9 +0,0 @@
-package org.rea;
-
-// TODO: tutaj wpisujemy dane do swojej bazy danych PostgreSQL
-
-public class PostgresConfig {
-    public static String url        = "jdbc:postgresql://localhost/postgres";
-    public static String user       = "rafal";
-    public static String password   = "postgre";
-}
