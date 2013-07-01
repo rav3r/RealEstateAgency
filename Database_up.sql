3aced8843dba67e40c4ceda2f70ce04df31e39d0
gaxit <gaxit@interia.eu>
2013-07-01 20:14:34 +0200

    Database_up - correct

--- Database_up.sql
+++ Database_up.sql
@@ -1,11 +1,7 @@
 DROP TABLE ulubione;
-DROP TABLE obrazki;
-DROP TABLE tagoferta;
 DROP TABLE oferty;
 DROP TABLE users;
 DROP TABLE adres;
-DROP TABLE tagi;
-DROP TABLE typy_umow;
 DROP TABLE typy_domow;
 
 
@@ -17,31 +13,13 @@ CREATE TABLE typy_domow
 );
 --------------------------------------------
 
-CREATE TABLE typy_umow
-(
-	id_typu_umowy serial PRIMARY KEY,
-	typ_umowy VARCHAR(30) NOT NULL
-);
-
-
---------------------------------------------
-CREATE TABLE tagi
-(
-	id_tagu serial PRIMARY KEY,
-	tresc VARCHAR(40) NOT NULL
-);
-
---------------------------------------------
 
 CREATE TABLE adres
 (
 	id_adresu serial PRIMARY KEY,
-	wojewodztwo VARCHAR(30) NOT NULL,
 	miasto VARCHAR(30) NOT NULL,
-	kod VARCHAR(6) NOT NULL,
 	ulica VARCHAR(30) NOT NULL,
-	nr_domu INTEGER NOT NULL,
-	nr_mieszkania INTEGER
+	nr_domu INTEGER NOT NULL
 );
 
 --------------------------------------------
@@ -54,8 +32,7 @@ CREATE TABLE users
 	imie VARCHAR(20)                NOT NULL    DEFAULT 'John',
 	nazwisko VARCHAR(30)            NOT NULL    DEFAULT 'Doe',
 	telefon VARCHAR(15)             NOT NULL    DEFAULT '123-123-412',
-	mail VARCHAR(30)                NOT NULL    DEFAULT 'jdoe@example.com',
-	pesel VARCHAR(11)               NOT NULL    DEFAULT '91061012345'
+	mail VARCHAR(30)                NOT NULL    DEFAULT 'jdoe@example.com'
 );
 
 --------------------------------------------
@@ -67,30 +44,9 @@ CREATE TABLE oferty
 	powierzchnia INTEGER NOT NULL,
 	data_dodania DATE NOT NULL,
 	id_typu_domu INTEGER REFERENCES typy_domow(id_typu_domu) NOT NULL,
-	id_typu_umowy INTEGER REFERENCES typy_umow(id_typu_umowy) NOT NULL,
 	id_adresu INTEGER REFERENCES adres(id_adresu) NOT NULL,
 	opis VARCHAR(500),
-	uwagi VARCHAR(200),
-	rynek VARCHAR(15) NOT NULL CHECK(rynek in ('pierwotny', 'wtorny')), --pierwotny/wtory - check
 	wlasciciel VARCHAR(32) REFERENCES users(login) NOT NULL
-);
-
---------------------------------------------
-
-CREATE TABLE tagoferta
-(
-	id_tagu INTEGER REFERENCES tagi(id_tagu) NOT NULL,
-	id_oferty INTEGER REFERENCES oferty(id_oferty) NOT NULL,
-	PRIMARY KEY (id_tagu, id_oferty)
-);
-
---------------------------------------------
-
-CREATE TABLE obrazki
-(
-	id_obrazka serial PRIMARY KEY,
-	id_oferty INTEGER REFERENCES oferty(id_oferty) NOT NULL,
-	link VARCHAR(150) NOT NULL
 );
 
 --------------------------------------------
