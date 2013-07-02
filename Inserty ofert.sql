0287db5c92a08cb0171c1e1422734792963eb330
gaxit <gaxit@interia.eu>
2013-07-02 16:07:24 +0200

    Inserty ofert fix

--- Inserty ofert.sql
+++ Inserty ofert.sql
@@ -1,19 +1,12 @@
 ﻿DELETE FROM oferty;
 DELETE FROM typy_domow;
-DELETE FROM typy_umow;
-DELETE FROM tagi;
 DELETE FROM adres;
+
+INSERT INTO users VALUES ('cyprian', 'haslo', DEFAULT, DEFAULT, DEFAULT, DEFAULT, DEFAULT);
 
 
 INSERT INTO typy_domow VALUES (1, 'wolnostojacy');
 SELECT * FROM typy_domow;
-
-INSERT INTO typy_umow VALUES (1, 'cywilno-prawna');
-SELECT * FROM typy_umow;
-
-INSERT INTO tagi VALUES (1, 'maly');
-INSERT INTO tagi VALUES (2, 'duzy');
-SELECT * FROM tagi;
 
 INSERT INTO adres VALUES(1, 'malopolskie', 'Krakow', '30-000', 'Florianska', 10);
 SELECT * FROM adres;
@@ -32,6 +25,3 @@ SELECT * FROM adres;
 
 INSERT INTO oferty VALUES
 (2, 400000, 60, current_date, 2, 1, 2, 'Opis', 'Uwagi', 'pierwotny', 'cyprian');
-SELECT * FROM oferty;
-
---SELECT * FROM users;
